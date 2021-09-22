82974f656a2fbf69c9c31bb1f1aebd8262e7bf88
Timo Dokter <timo.dokter@student.rocvf.nl>
2021-09-22 11:55:59 +0200

    the header for the home screen is finished

---
+++ index.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Star Wars fan wiki</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="Styles/MainStyleSheet.css" rel="stylesheet" type="text/css">
+    <link href="images/lightsaber%20icon.png" type="image/icon" rel="icon">
+</head>
+<body>
+<header>
+    <h1 class="mainPageTitle">Star Wars Wiki</h1>
+    <nav>
+        <a href="WebPages/movies.php">Movies</a>
+        <a href="WebPages/series.php">Series</a>
+        <a href="WebPages/characters.php">Characters</a>
+        <a href="WebPages/registration.php">Registration</a>
+    </nav>
+</header>
+<?php
+
+?>
+</body>
+</html>
